70c5f62d47037ad3157b3c6f00d8a5c3c9f29ace
LynXHashib <hashib4305@gmail.com>
2025-06-25 12:39:37 +0600

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/SocialMedia/',
+  build: {
+    outDir: 'dist',
+  },
+
   server: {
     port: 5001,
   },
